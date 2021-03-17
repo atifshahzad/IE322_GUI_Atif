0259f9ff1a312d0807656e3717c52a3c71ea719b
Atif SHAHZAD <atifshahzad@gmail.com>
2021-03-17 10:50:06 +0300

    updated

--- MyFormApp2/frmArray.Designer.cs
+++ MyFormApp2/frmArray.Designer.cs
@@ -209,7 +209,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 439);
+            this.ClientSize = new System.Drawing.Size(839, 438);
             this.Controls.Add(this.BtnDisplayPropertiesMethods1D);
             this.Controls.Add(this.BtnQuestion02_2D);
             this.Controls.Add(this.BtnReset);
