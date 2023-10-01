5df6793059b4b7a0aaaa632a477251515fb8dbec
aeshsplash <aeshsplash@Gmail.com>
2023-10-01 19:21:34 +0300

    Console Project Created

---
+++ IE322_ConsoleApp_KAU_DrAtif/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IE322_ConsoleApp_KAU_DrAtif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
