4b2f7be3e63540bc989d276750118c3da1eba249
aeshsplash <aeshsplash@Gmail.com>
2024-02-12 01:35:19 +0300

    A python project is added in the solution

---
+++ IE322_PythonApp_DrAtif/IE322_PythonApp_DrAtif.py
@@ -0,0 +1,45 @@
+
+# Python
+my_int = 10
+my_float = 3.14
+my_string = "Hello"
+
+print(my_int)
+print(my_float)
+print(my_string)
+
+
+
+# Python
+x = 10
+
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+    
+
+# Python
+for i in range(5):
+    print(i)
+
+j = 0
+while j < 5:
+    print(j)
+    j += 1
+
+
+
+# Python
+def add(a, b):
+    return a + b
+
+print(add(3, 5))
+print(add(10, 20))
+
+
+# Python
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    print(num)
